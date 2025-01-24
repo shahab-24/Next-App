ee43fbfe88e52b18e3c03ea7ebbb7ed5aa7a3fec
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-01-24 12:32:20 +0600

    error

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+        content: [
+                "./app/**/*.{js,ts,jsx,tsx}",
+                "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+                "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+                "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Ensure you are targeting all your component files
+              ],
   theme: {
     extend: {
       colors: {
@@ -16,6 +16,6 @@ export default {
     },
   },
   plugins: [
-    require('daisyui')
-  ],
+        require('daisyui'), 
+      ],
 };
